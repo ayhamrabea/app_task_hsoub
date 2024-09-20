e380e29cf3593b123d28cdce3546cc26d8df50e4
ayhamrabea <ayhamrabea05@.com>
2024-09-20 11:49:23 +0300

    add arabic la

--- project/projects/views.py
+++ project/projects/views.py
@@ -3,19 +3,23 @@ from .models import Project , Category , Task
 from .forms import ProjectCreateForm , ProjectUpdateForm
 from django.shortcuts import render , redirect
 from django.urls import reverse_lazy , reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 
 
-class ProjectListView(LoginRequiredMixin , ListView):
+
+# (4) LoginRequiredMixin     لكي نتأكد من تسجيل الدخول لكي لا يعرض المعلومات
+# (5)  UserPassesTestMixin     لكي نتأكد من أن المشروع معروض من قبل صاحبه فقط
+ 
+
+
+class ProjectListView(LoginRequiredMixin , ListView):  #  (4) LoginRequiredMixin   نقوم بتوريثها في كل الدوال
     model = Project
     template_name = 'project/list.html'
     paginate_by = 6
 
     def  get_queryset(self):
         query_set = super().get_queryset()
-        where = {}
+        where = {'user_id' : self.request.user}   # قمنا بكتابة شرط لكي يظهر مشاريع المستخدم فقط
         q = self.request.GET.get('q',None)
         if q:where['titel'] = q
         return query_set.filter(**where)
@@ -28,16 +32,27 @@ class ProjectCreateView(LoginRequiredMixin , CreateView):
     template_name = 'project/create.html'
     success_url = reverse_lazy('Project_list')
 
+    def form_valid(self, form):   #   قمنا بكتابة دالة لكي يأخد المشروع id ال user
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
 
 
 
-class ProjectUpdateView(LoginRequiredMixin , UpdateView):
+
+class ProjectUpdateView(LoginRequiredMixin , UserPassesTestMixin , UpdateView):
     model = Project
     form_class = ProjectUpdateForm
     template_name = 'project/update.html'
+
+    def test_func(self):    # (5) لكي نتحقق من صاحب المشروع و المستخدم الحالي
+        return self.get_object().user_id == self.request.user.id   #  when this function returns false the page doesn't work
+
     
     def get_success_url(self) :
         return reverse('Project_Update' , args=[self.object.id])
+
+
 
 
 class ProjectDeleteView(LoginRequiredMixin , DeleteView):
